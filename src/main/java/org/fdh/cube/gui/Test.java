0bf5f9132508a1d86647e8f8d06fcf4db5a02e0a
sunjianglong <sunjianglong@1m2.net>
2019-09-27 13:34:48 +0800

    xxx

--- src/main/java/org/fdh/cube/gui/Test.java
+++ src/main/java/org/fdh/cube/gui/Test.java
@@ -6,12 +6,16 @@ import java.util.HashMap;
 import org.fdh.cube.model.FormulaThread;
 import org.fdh.cube.model.KeyboardLitenerTest;
 import org.fdh.cube.model.ThirdOrderCube;
+import org.fdh.cube.thread.CMDThread;
 
 import com.alibaba.fastjson.JSONObject;
 
 
 public class Test{
     public static void main(String[] args) {
-        KeyboardLitenerTest Test = new KeyboardLitenerTest();
+        //KeyboardLitenerTest Test = new KeyboardLitenerTest();
+        CMDThread runnable=new CMDThread();
+        Thread thread=new Thread(runnable);
+        thread.start();
     }
 }
