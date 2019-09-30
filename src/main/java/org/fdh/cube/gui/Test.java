4c5d1a05a4a0597757786ca50a71ecb4a8cb0ce6
sunjianglong <sunjianglong@1m2.net>
2019-09-30 12:02:11 +0800

    xxx

--- src/main/java/org/fdh/cube/gui/Test.java
+++ src/main/java/org/fdh/cube/gui/Test.java
@@ -1,22 +1,15 @@
 package org.fdh.cube.gui;
 
-
-import java.util.HashMap;
-
-import org.fdh.cube.model.FormulaThread;
-import org.fdh.cube.model.KeyboardLitenerTest;
-import org.fdh.cube.model.ThirdOrderCube;
-import org.fdh.cube.thread.CMDThread;
-
-import com.alibaba.fastjson.JSONObject;
+import org.fdh.cube.util.Environment;
 
 
 public class Test{
-    public static void main(String[] args) {
+    public static void main(String[] args) throws ClassNotFoundException {
 //        //KeyboardLitenerTest Test = new KeyboardLitenerTest();
 //        CMDThread runnable=new CMDThread();
 //        Thread thread=new Thread(runnable);
 //        thread.start();
-        
+        ClassLoader.getSystemClassLoader().loadClass("org.fdh.cube.util.Environment");
+        System.out.println(Environment.get("user.dir"));
     }
 }
