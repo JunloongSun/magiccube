b56e9bec0e1d5661744187245cddf630a178f45e
sunjianglong <sunjianglong@1m2.net>
2019-10-11 14:44:59 +0800

    没用的

---
+++ src/main/java/org/fdh/cube/searchEngine/Test.java
@@ -0,0 +1,52 @@
+package org.fdh.cube.searchEngine;
+
+import java.io.File;
+import java.io.IOException;
+
+import org.apache.lucene.analysis.Analyzer;
+import org.apache.lucene.analysis.standard.StandardAnalyzer;
+import org.apache.lucene.document.Document;
+import org.apache.lucene.document.StringField;
+import org.apache.lucene.document.TextField;
+import org.apache.lucene.index.IndexWriter;
+import org.apache.lucene.index.IndexWriterConfig;
+import org.apache.lucene.store.Directory;
+import org.apache.lucene.store.FSDirectory;
+import org.apache.lucene.util.Version;
+import org.apache.lucene.document.Field;
+
+public class Test {
+    /** 创建索引 */
+    public static void createIndex() throws IOException {
+        //1 创建文档对象
+        Document document = new Document();
+        // 创建并添加字段信息。参数：字段的名称、字段的值、是否存储，这里选Store.YES代表存储到文档列表。Store.NO代表不存储
+        document.add(new StringField("id", "1", Field.Store.YES));
+        // 这里我们title字段需要用TextField，即创建索引又会被分词。StringField会创建索引，但是不会被分词
+        document.add(new TextField("title", "谷歌地图之父跳槽facebook", Field.Store.YES));
+
+        //2 索引目录类,指定索引在硬盘中的位置
+        Directory directory = FSDirectory.open(new File(System.getProperty("user.dir")+"\\indexDir"));
+        //3 创建分词器对象
+        Analyzer analyzer = new StandardAnalyzer();
+        //4 索引写出工具的配置对象
+        IndexWriterConfig conf = new IndexWriterConfig(Version.LATEST, analyzer);
+        //5 创建索引的写出工具类。参数：索引的目录和配置信息
+        IndexWriter indexWriter = new IndexWriter(directory, conf);
+
+        //6 把文档交给IndexWriter
+        indexWriter.addDocument(document);
+        //7 提交
+        indexWriter.commit();
+        //8 关闭
+        indexWriter.close();
+    }
+    public static void main(String[] args){
+        try {
+            createIndex();
+        } catch (IOException e) {
+            // TODO Auto-generated catch block
+            e.printStackTrace();
+        }
+    }
+}
